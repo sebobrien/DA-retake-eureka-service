ba717d47fba8427f433ca86f4e3bb4ac924fc0ee
sebobrien <sebobrien@gmail.com>
2018-08-08 14:11:25 +0200

    last check before handing in

--- src/main/java/com/seb/eureakaservice/EureakaServiceApplication.java
+++ src/main/java/com/seb/eureakaservice/EureakaServiceApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import com.seb.movieservice.MovieController;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EureakaServiceApplication {
 	
-	MovieController movie = new MovieController();
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EureakaServiceApplication.class, args);
 	}
